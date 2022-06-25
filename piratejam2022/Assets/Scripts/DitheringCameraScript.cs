42740b189cb4a1ddea020a57127f6120f45d5f24
ashleycrouch <ashley.crouch@rocketmail.com>
2022-06-24 22:05:12 -0500

    Initial commit

---
+++ piratejam2022/Assets/Scripts/DitheringCameraScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DitheringCameraScript : MonoBehaviour
+{
+    public Material ditherMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Creates a material for dithering with.
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        // RenderTexture main = RenderTexture.GetTemporary(820, 470);
+        // Graphics.Blit(src, main, ditherMat);
+        // Graphics.Blit(main, dest);
+
+        // RenderTexture.ReleaseTemporary(main);
+
+        Graphics.Blit(src, dest, ditherMat);
+    }
+}
