133ea822113224ff81ff65337d8d8ab8092a1300
ashleycrouch <ashley.crouch@rocketmail.com>
2022-06-26 15:04:47 -0500

    Cleaning up shader experiments.

--- piratejam2022/Assets/Scripts/DitheringCameraScript.cs
+++ piratejam2022/Assets/Scripts/DitheringCameraScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
 public class DitheringCameraScript : MonoBehaviour
 {
     public Material ditherMat;
