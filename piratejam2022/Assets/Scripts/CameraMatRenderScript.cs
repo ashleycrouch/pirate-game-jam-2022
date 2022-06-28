89bdbd47234dd3f3abbe9a05c7e37b9e43eefc40
ashleycrouch <ashley.crouch@rocketmail.com>
2022-06-27 23:18:02 -0500

    Shader kind of works. Still not dithering, though.

--- piratejam2022/Assets/Scripts/CameraMatRenderScript.cs
+++ piratejam2022/Assets/Scripts/CameraMatRenderScript.cs
@@ -7,21 +7,36 @@ using UnityEngine;
 public class DitheringCameraScript : MonoBehaviour
 {
     public Material ditherMat;
+    public Material thresholdMat;
+    public Camera camera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = GetComponent<Camera>();
     }
 
     // Creates a material for dithering with.
     void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        // RenderTexture main = RenderTexture.GetTemporary(820, 470);
-        // Graphics.Blit(src, main, ditherMat);
-        // Graphics.Blit(main, dest);
+        RenderTexture large = RenderTexture.GetTemporary(1640, 940, 0, RenderTextureFormat.ARGB32);
+        RenderTexture main = RenderTexture.GetTemporary(820, 470, 0, RenderTextureFormat.ARGB32);
+        large.filterMode = FilterMode.Bilinear;
+        main.filterMode = FilterMode.Bilinear;
 
-        // RenderTexture.ReleaseTemporary(main);
+        Vector3[] corners = new Vector3[4];
+        camera.CalculateFrustumCorners(new Rect(0,0,1,1), camera.farClipPlane, Camera.MonoOrStereoscopicEye.Mono, corners);
 
-        Graphics.Blit(src, dest, ditherMat);
+        for(int i = 0; i < 4; i++) {
+            corners[i] = transform.TransformVector(corners[i]);
+            corners[i].Normalize();
+        }
+        Graphics.Blit(src, large, ditherMat);
+        Graphics.Blit(large, main, thresholdMat);
+        //Graphics.Blit(src, normal, mat);
+        Graphics.Blit(main, dest);
+
+        RenderTexture.ReleaseTemporary(large);
+        RenderTexture.ReleaseTemporary(main);
     }
 }
